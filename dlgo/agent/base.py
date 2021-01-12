0008ec68324b6d53c4dcc5aac2b7b0b3c06311a5
hans1996 <hans8512185121@gmail.com>
2021-01-12 17:08:21 +0800

    finished basic 30k level machine

---
+++ dlgo/agent/base.py
@@ -0,0 +1,10 @@
+class Agent:
+    def __init__(self):
+        pass
+
+    def select_move(self, game_state):
+        raise NotImplementedError()
+# end::agent[]
+
+    def diagnostics(self):
+        return {}
---
+++ test.py
@@ -0,0 +1,11 @@
+
+from dlgo import agent
+from dlgo import goboard_slow as goboard
+from dlgo import gotypes
+from dlgo.utils import print_board, print_move
+import time
+
+gotypes.Player.black
+
+
+agent.naive
